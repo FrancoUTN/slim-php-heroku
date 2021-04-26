8400409e228947409ea01add7c91c5d7cf7321be
unknown <cataniafrancoar@gmail.com>
2021-04-26 17:32:26 -0300

    cuarto commit

--- app/index.php
+++ app/index.php
@@ -10,10 +10,11 @@ $app = new \Slim\App([]);
 
 
 
-$app->get('[/]', function (Request $request, Response $response) {    
-    // $response->getBody()->write("<a href='./ejer6.php'>Ejercicio 6</a>");
-    $response->getBody()->write("<a href='./ejercicios.php'>Ejercicios de la Cursada</a>");
-    $response->getBody()->write("<a href='./parcial.php'>Ejercicios del Parcial</a>");
+$app->get('[/]', function (Request $request, Response $response) {
+    $response->getBody()->write("<ul>
+                                    <li><a href='./ejercicios'>Ejercicios de la Cursada</a></li>
+                                    <li><a href='./parcial'>Ejercicios del Parcial</a></li>
+                                </ul>");
     return $response;
 
 });
